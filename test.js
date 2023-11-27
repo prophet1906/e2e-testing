51a19852d03fa78da2ba78982c7bd1577ace4b92
prophet1906 <shubham.null@gmail.com>
2023-11-27 17:26:33 +0530

    Final Set of Tests Added

---
+++ test.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const async = require('async');
+const newman = require('newman');
+
+const testDir = path.join(__dirname, "__tests__");
+const reportDir = path.join(__dirname, "public", "reports");
+const environmentPath = path.join(__dirname, "env.json");
+
+function runTests() {
+    fs.readdir(testDir, (err, testCollections) => {
+        async.eachSeries(testCollections, (testCollection, next) => {
+            const collectionPath = path.join(testDir, testCollection);
+            const reportPath = path.join(reportDir, testCollection).replace('json', 'html');
+            newman.run({
+                collection: collectionPath,
+                environment: environmentPath,
+                reporters: ['cli', 'htmlextra'],
+                reporter: {
+                    htmlextra: {
+                        export: reportPath
+                    }
+                }
+            }).on('start', (err, args) => {
+                console.log(`Running collection ${testCollection}.`);
+            }).on('done', (err, summary) => {
+                if (err || summary.error) {
+                    console.error('Collection run encountered error.');
+                } else {
+                    console.log(`Collection ${testCollection} ran successfully!`)
+                }
+                next(err, summary);
+            })
+        }, (err, results) => {
+            if (err) console.error(err);
+            console.log(results);
+        });
+    });
+}
+
+runTests();
